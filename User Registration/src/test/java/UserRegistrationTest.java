2d05d7695e2d5fae3c4d7ad4b5a5b862592261ad
Upendra Sahu <usahu998@gmail.com>
2019-11-23 09:50:18 +0530

    Add: Last name test cases

--- User Registration/src/test/java/UserRegistrationTest.java
+++ User Registration/src/test/java/UserRegistrationTest.java
@@ -1,12 +1,18 @@
 import com.bridgelabz.junit.UserRegistration;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 
 public class UserRegistrationTest {
 
-    UserRegistration userRegistration = new UserRegistration();
+    UserRegistration userRegistration;
+
+    @Before
+    public void  initialize(){
+        userRegistration = new UserRegistration();
+    }
 
     @Test
     public void givenFirstName_WhenProper_ShouldReturnTrue() {
@@ -59,9 +65,50 @@ public class UserRegistrationTest {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
-      //  UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validateLastName("Lastname");
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldMinimumThreeChar_ReturnTrue() {
+        Assert.assertTrue(userRegistration.validateLastName("Upendra"));
+    }
+
+    @Test
+    public void givenLastName_WhenImproper_ShouldMinimumThreeChar_ReturnFail() {
+        Assert.assertFalse(userRegistration.validateLastName("Up"));
+    }
+
+    @Test
+    public void givenLastName_WhenImproper_ShouldNotNumeric_ReturnFail() {
+        Assert.assertFalse(userRegistration.validateLastName("Up123124"));
+    }
+
+    @Test
+    public void givenLastName_WhenImproper_ShouldSpace_ReturnFail() {
+        Assert.assertFalse(userRegistration.validateLastName("Upe ndra"));
+    }
+
+    @Test
+    public void givenLastName_WhenImproper_ShouldNotAnySpecialSymbol_ReturnFalse() {
+        Assert.assertFalse(userRegistration.validateLastName("Upe@ndra"));
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldNotSpace_ReturnTrue() {
+        Assert.assertTrue(userRegistration.validateLastName("Upendra"));
+    }
+
+    @Test
+    public void givenLastName_WhenProper_ShouldFirstCharCapital_ReturnTrue() {
+        Assert.assertTrue(userRegistration.validateLastName("Upendra"));
+    }
+
+    @Test
+    public void givenLastName_WhenImproper_ShouldFirstCharCapital_ReturnFalse() {
+        Assert.assertFalse(userRegistration.validateLastName("upendra"));
+    }
+
+
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
