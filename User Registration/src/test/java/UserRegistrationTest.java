58c1959eb89c1db6dbedea86c96688b93fff6551
Upendra Sahu <usahu998@gmail.com>
2019-11-23 09:35:21 +0530

    Refactoring Method name of FirstName

--- User Registration/src/test/java/UserRegistrationTest.java
+++ User Registration/src/test/java/UserRegistrationTest.java
@@ -18,49 +18,49 @@ public class UserRegistrationTest {
     }
 
     @Test
-    public void givenFirstName_WhenProper_ShouldMinimumThreeCharTrue() {
+    public void givenFirstName_WhenProper_ShouldMinimumThreeChar_ReturnTrue() {
         Assert.assertTrue(userRegistration.validateFirstName("Upendra"));
     }
 
     @Test
-    public void givenFirstName_WhenImproper_ShouldMinimumThreeCharFail() {
+    public void givenFirstName_WhenImproper_ShouldMinimumThreeChar_ReturnFail() {
         Assert.assertFalse(userRegistration.validateFirstName("Up"));
     }
 
     @Test
-    public void givenFirstName_WhenImproper_ShouldNotNumericFail() {
+    public void givenFirstName_WhenImproper_ShouldNotNumeric_ReturnFail() {
         Assert.assertFalse(userRegistration.validateFirstName("Up123124"));
     }
 
     @Test
-    public void givenFirstName_WhenImproper_ShouldSpaceFail() {
+    public void givenFirstName_WhenImproper_ShouldSpace_ReturnFail() {
         Assert.assertFalse(userRegistration.validateFirstName("Upe ndra"));
     }
 
     @Test
-    public void givenFirstName_WhenImproper_ShouldNotAnySpecialSymbolFalse() {
+    public void givenFirstName_WhenImproper_ShouldNotAnySpecialSymbol_ReturnFalse() {
         Assert.assertFalse(userRegistration.validateFirstName("Upe@ndra"));
     }
 
     @Test
-    public void givenFirstName_WhenProper_ShouldNotSpaceTrue() {
+    public void givenFirstName_WhenProper_ShouldNotSpace_ReturnTrue() {
         Assert.assertTrue(userRegistration.validateFirstName("Upendra"));
     }
 
     @Test
-    public void givenFirstName_WhenProper_ShouldFirstCharCapitalTrue() {
+    public void givenFirstName_WhenProper_ShouldFirstCharCapital_ReturnTrue() {
         Assert.assertTrue(userRegistration.validateFirstName("Upendra"));
     }
 
     @Test
-    public void givenFirstName_WhenImproper_ShouldFirstCharCapitalFalse() {
+    public void givenFirstName_WhenImproper_ShouldFirstCharCapital_ReturnFalse() {
         Assert.assertFalse(userRegistration.validateFirstName("upendra"));
     }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
       //  UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.validateLastName("Lastame");
+        boolean result = userRegistration.validateLastName("Lastname");
         Assert.assertTrue(result);
     }
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
